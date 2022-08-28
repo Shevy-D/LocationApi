a32415bfcaf5d33809b81408a60218b55dbde9c5
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-28 13:52:42 +0500

    Added methods: startLocationUpdates

--- app/src/main/java/com/shevy/locationapi/MainActivity.kt
+++ app/src/main/java/com/shevy/locationapi/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.shevy.locationapi
 
+import android.Manifest
+import android.content.IntentSender.SendIntentException
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.shevy.locationapi.databinding.ActivityMainBinding
 import java.text.DateFormat
@@ -13,6 +21,9 @@ import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
+    private val CHECK_SETTINGS_CODE = 111
+    private val REQUEST_LOCATION_PERMISSION = 222
+
     lateinit var binding: ActivityMainBinding
     private lateinit var startLocationUpdatesButton: Button
     private lateinit var stopLocationUpdatesButton: Button
@@ -41,9 +52,61 @@ class MainActivity : AppCompatActivity() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         settingsClient = LocationServices.getSettingsClient(this)
 
+        startLocationUpdatesButton.setOnClickListener {
+            startLocationUpdates()
+        }
+
         buildLocationRequest()
         buildLocationCallBack()
         buildLocationSettingsRequest()
+    }
+
+    private fun startLocationUpdates() {
+        isLocationUpdatesActive = true
+        startLocationUpdatesButton.isEnabled = false
+        stopLocationUpdatesButton.isEnabled = true
+
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+            .addOnSuccessListener(this) { locationSettingsResponse: LocationSettingsResponse? ->
+            if (ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return@addOnSuccessListener
+            }
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallBack,
+                Looper.myLooper()
+            )
+            updateLocationUi()
+        }.addOnFailureListener { e: Exception ->
+            val statusCode = (e as ApiException).statusCode
+            when (statusCode) {
+                LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                    val resolvableApiException = e as ResolvableApiException
+                    resolvableApiException.startResolutionForResult(
+                        this@MainActivity,
+                        CHECK_SETTINGS_CODE
+                    )
+                } catch (sie: SendIntentException) {
+                    sie.printStackTrace()
+                }
+                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                    val message = "Adjust location settings on your device"
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                    isLocationUpdatesActive = false
+                    startLocationUpdatesButton.isEnabled = true
+                    stopLocationUpdatesButton.isEnabled = false
+                }
+            }
+            updateLocationUi()
+        }
     }
 
     private fun buildLocationSettingsRequest() {
@@ -58,10 +121,14 @@ class MainActivity : AppCompatActivity() {
                 super.onLocationResult(p0)
                 currentLocation = p0.lastLocation!!
 
-                locationTextView.text = "${currentLocation.latitude}/${currentLocation.longitude}"
-                locationUpdateTimeTextView.text = "${DateFormat.getDateInstance().format(Date())}"
+                updateLocationUi()
             }
         }
+    }
+
+    private fun updateLocationUi() {
+        locationTextView.text = "${currentLocation.latitude}/${currentLocation.longitude}"
+        locationUpdateTimeTextView.text = "${DateFormat.getDateInstance().format(Date())}"
     }
 
     private fun buildLocationRequest() {

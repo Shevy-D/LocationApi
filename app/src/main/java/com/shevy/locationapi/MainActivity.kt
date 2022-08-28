1aa1cf67539eaf840400205c4ff9a781c62d04a7
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-08-28 15:20:57 +0500

    Finished the app

--- app/src/main/java/com/shevy/locationapi/MainActivity.kt
+++ app/src/main/java/com/shevy/locationapi/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.shevy.locationapi
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.IntentSender.SendIntentException
 import android.content.pm.PackageManager
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -14,10 +20,10 @@ import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.google.android.material.snackbar.Snackbar
 import com.shevy.locationapi.databinding.ActivityMainBinding
 import java.text.DateFormat
 import java.util.*
-import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
@@ -35,9 +41,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationSettingsRequest: LocationSettingsRequest
     private lateinit var locationCallBack: LocationCallback
-    private lateinit var currentLocation: Location
+    private var currentLocation: Location? = null
 
-    private var isLocationUpdatesActive by Delegates.notNull<Boolean>()
+    private var isLocationUpdatesActive = false
     private lateinit var locationUpdateTime: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +54,7 @@ class MainActivity : AppCompatActivity() {
         startLocationUpdatesButton = binding.startLocationUpdatesButton
         stopLocationUpdatesButton = binding.stopLocationUpdatesButton
         locationTextView = binding.locationTextView
+        locationUpdateTimeTextView = binding.locationUpdateTimeTextView
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         settingsClient = LocationServices.getSettingsClient(this)
@@ -56,9 +63,24 @@ class MainActivity : AppCompatActivity() {
             startLocationUpdates()
         }
 
+        stopLocationUpdatesButton.setOnClickListener {
+            stopLocationUpdates()
+        }
+
         buildLocationRequest()
         buildLocationCallBack()
         buildLocationSettingsRequest()
+    }
+
+    private fun stopLocationUpdates() {
+        if (!isLocationUpdatesActive) {
+            return
+        }
+        fusedLocationClient.removeLocationUpdates(locationCallBack).addOnCompleteListener(this) {
+            isLocationUpdatesActive = false
+            startLocationUpdatesButton.isEnabled = true
+            stopLocationUpdatesButton.isEnabled = false
+        }
     }
 
     private fun startLocationUpdates() {
@@ -68,44 +90,63 @@ class MainActivity : AppCompatActivity() {
 
         settingsClient.checkLocationSettings(locationSettingsRequest)
             .addOnSuccessListener(this) { locationSettingsResponse: LocationSettingsResponse? ->
-            if (ActivityCompat.checkSelfPermission(
-                    this@MainActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(
-                    this@MainActivity,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return@addOnSuccessListener
-            }
-            fusedLocationClient.requestLocationUpdates(
-                locationRequest,
-                locationCallBack,
-                Looper.myLooper()
-            )
-            updateLocationUi()
-        }.addOnFailureListener { e: Exception ->
-            val statusCode = (e as ApiException).statusCode
-            when (statusCode) {
-                LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
-                    val resolvableApiException = e as ResolvableApiException
-                    resolvableApiException.startResolutionForResult(
+                if (ActivityCompat.checkSelfPermission(
                         this@MainActivity,
-                        CHECK_SETTINGS_CODE
-                    )
-                } catch (sie: SendIntentException) {
-                    sie.printStackTrace()
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return@addOnSuccessListener
                 }
-                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
-                    val message = "Adjust location settings on your device"
-                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                fusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallBack,
+                    Looper.myLooper()
+                )
+                updateLocationUi()
+            }.addOnFailureListener { e: Exception ->
+                val statusCode = (e as ApiException).statusCode
+                when (statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                        val resolvableApiException = e as ResolvableApiException
+                        resolvableApiException.startResolutionForResult(
+                            this@MainActivity,
+                            CHECK_SETTINGS_CODE
+                        )
+                    } catch (sie: SendIntentException) {
+                        sie.printStackTrace()
+                    }
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                        val message = "Adjust location settings on your device"
+                        Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                        isLocationUpdatesActive = false
+                        startLocationUpdatesButton.isEnabled = true
+                        stopLocationUpdatesButton.isEnabled = false
+                    }
+                }
+                updateLocationUi()
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === CHECK_SETTINGS_CODE) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    Log.d("MainActivity", "User has agreed to change location settings")
+                    startLocationUpdates()
+                }
+                RESULT_CANCELED -> {
+                    Log.d("MainActivity", "User has not agreed to change location settings")
                     isLocationUpdatesActive = false
                     startLocationUpdatesButton.isEnabled = true
                     stopLocationUpdatesButton.isEnabled = false
+                    updateLocationUi()
                 }
             }
-            updateLocationUi()
         }
     }
 
@@ -117,24 +158,98 @@ class MainActivity : AppCompatActivity() {
 
     private fun buildLocationCallBack() {
         locationCallBack = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                super.onLocationResult(p0)
-                currentLocation = p0.lastLocation!!
-
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                currentLocation = locationResult.lastLocation!!
                 updateLocationUi()
             }
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun updateLocationUi() {
-        locationTextView.text = "${currentLocation.latitude}/${currentLocation.longitude}"
-        locationUpdateTimeTextView.text = "${DateFormat.getDateInstance().format(Date())}"
+        if (currentLocation != null) {
+            locationTextView.text = "${currentLocation!!.latitude}/${currentLocation!!.longitude}"
+            locationUpdateTimeTextView.text = DateFormat.getTimeInstance(DateFormat.MEDIUM).format(Date())
+        }
     }
 
     private fun buildLocationRequest() {
         locationRequest = LocationRequest()
-        locationRequest.interval = 10000
-        locationRequest.fastestInterval = 3000
+        locationRequest.interval = 2500
+        locationRequest.fastestInterval = 1000
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isLocationUpdatesActive && checkLocationPermission()) {
+            startLocationUpdates();
+        } else if (!checkLocationPermission()) {
+            requestLocationPermission();
+        }
+    }
+
+    private fun requestLocationPermission() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (shouldProvideRationale) {
+            showSnackBar(
+                "Location permission is needed for app functionality",
+                "OK"
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION_PERMISSION
+                )
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    private fun showSnackBar(mainText: String, action: String, listener: View.OnClickListener) {
+        Snackbar.make(findViewById(android.R.id.content), mainText, Snackbar.LENGTH_INDEFINITE)
+            .setAction(action, listener).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isEmpty()) {
+                Log.d("permissionResult", "Request wag cancelled")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (isLocationUpdatesActive) {
+                    startLocationUpdates()
+                }
+            } else {
+                showSnackBar("Turn on location on settings", "Settings") {
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    val uri: Uri =
+                        Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                    intent.data = uri
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        val permissionState =
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        return permissionState == PackageManager.PERMISSION_GRANTED
     }
 }
